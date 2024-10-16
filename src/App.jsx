6266e0d0333950f98b194fd63799d53594432f6f
Hassan Shaikh <thehasanshaikh@gmail.com>
2024-10-16 20:15:18 +0530

    centered div

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import LatestPost from "./components/LatestPost";
 function App() {
   return (
     <>
-      <section className="w-full flex px-20">
-        <div className="w-2/6">
+      <section className="w-full flex justify-center">
+        <div className="w-3/12 px-2">
           <CategoryList />
         </div>
-        <div className="w-2/6">
+        <div className="w-3/12 px-2">
           <BlogList />
         </div>
-        <div className="w-2/6">
+        <div className="w-3/12 px-2">
           <LatestPost />
         </div>
       </section>
