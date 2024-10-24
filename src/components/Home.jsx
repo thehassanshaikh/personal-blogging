6d5eb35a049dda05260f77047daaf84aadf12366
Hassan Shaikh <thehasanshaikh@gmail.com>
2024-10-24 20:07:48 +0530

    added navigation bar and styled it for the desktop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,24 @@
 import CategoryList from "./CategoryList";
 import LatestPost from "./LatestPost";
 import BlogLists from "./BlogLists";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
-    <section className="w-full flex justify-center">
-      <div className="w-2/12 px-2">
-        <CategoryList />
-      </div>
-      <div className="w-5/12 px-2">
-        <BlogLists />
-      </div>
-      <div className="w-2/12 px-2">
-        <LatestPost />
-      </div>
-    </section>
+    <>
+      <Navbar />
+      <section className="w-full flex justify-center">
+        <div className="w-2/12 px-2">
+          <CategoryList />
+        </div>
+        <div className="w-5/12 px-2">
+          <BlogLists />
+        </div>
+        <div className="w-2/12 px-2">
+          <LatestPost />
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-white mb-4">
+      <div className="flex m-auto justify-between w-9/12 p-2">
+        <div>
+          <img
+            className="w-10 h-10"
+            src="https://cdn.pixabay.com/photo/2022/01/16/16/44/blogger-logo-6942640_1280.png"
+            alt=""
+          />
+        </div>
+        <div className="w-8/12 m-auto">
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search"
+            required
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
